8418a104e2cfe1fbcd239a39b4144e800967cd83
admin <kalrobbynson@gmail.com>
2020-09-21 22:06:20 +0300

    project structure

---
+++ lib/App.dart
@@ -0,0 +1,17 @@
+import 'package:aunty_rafiki/views/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
