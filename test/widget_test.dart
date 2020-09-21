dab1d7a53606ec11e14d8940085635a9c194e740
admin <kalrobbynson@gmail.com>
2020-09-21 23:14:50 +0300

    tests removed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:aunty_rafiki/App.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
