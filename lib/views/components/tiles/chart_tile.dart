97201fbf697be04615ec873a4cd6c4d3e0968d88
admin <kalrobbynson@gmail.com>
2020-09-22 22:52:25 +0300

    chart tile created

---
+++ lib/views/components/tiles/chart_tile.dart
@@ -0,0 +1,80 @@
+import 'package:aunty_rafiki/models/chart.dart';
+import 'package:flutter/material.dart';
+
+class ChartTile extends StatelessWidget {
+  final Chart chart;
+
+  const ChartTile({Key key, @required this.chart}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            height: 100,
+            child: Row(children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: CircleAvatar(
+                  radius: 35,
+                  backgroundImage: AssetImage(chart.avatar),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Row(children: <Widget>[
+                        Expanded(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  chart.name,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                Text(chart.lastMessage)
+                              ]),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(chart.time,
+                                      style: TextStyle(
+                                          color: chart.messageCounter >
+                                                  0
+                                              ? Theme.of(context).primaryColor
+                                              : Colors.black38)),
+                                  chart.messageCounter > 0
+                                      ? Chip(
+                                          backgroundColor:
+                                              Theme.of(context).primaryColor,
+                                          label: Text(
+                                            chart
+                                                .messageCounter
+                                                .toString(),
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ))
+                                      : Container()
+                                ]),
+                          ),
+                        )
+                      ]),
+                    ),
+                    Divider()
+                  ],
+                ),
+              )
+            ]),
+          );
+        
+      
+  }
+}
--- lib/views/screens/chat_screen.dart
+++ lib/views/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:aunty_rafiki/models/chart.dart';
+import 'package:aunty_rafiki/views/components/tiles/chart_tile.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -7,74 +8,7 @@ class ChatScreen extends StatelessWidget {
     return ListView.builder(
         itemCount: chartList.length,
         itemBuilder: (_, index) {
-          return Container(
-            height: 100,
-            child: Row(children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: CircleAvatar(
-                  radius: 35,
-                  backgroundImage: AssetImage(chartList[index].avatar),
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Row(children: <Widget>[
-                        Expanded(
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  chartList[index].name,
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18),
-                                ),
-                                Text(chartList[index].lastMessage)
-                              ]),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 10),
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: <Widget>[
-                                  Text(chartList[index].time,
-                                      style: TextStyle(
-                                          color: chartList[index]
-                                                      .messageCounter >
-                                                  0
-                                              ? Theme.of(context).primaryColor
-                                              : Colors.black38)),
-                                  chartList[index].messageCounter > 0
-                                      ? Chip(
-                                          backgroundColor:
-                                              Theme.of(context).primaryColor,
-                                          label: Text(
-                                            chartList[index]
-                                                .messageCounter
-                                                .toString(),
-                                            style:
-                                                TextStyle(color: Colors.white),
-                                          ))
-                                      : Container()
-                                ]),
-                          ),
-                        )
-                      ]),
-                    ),
-                    Divider()
-                  ],
-                ),
-              )
-            ]),
-          );
+          return ChartTile(chart: chartList[index]);
         });
   }
 }
