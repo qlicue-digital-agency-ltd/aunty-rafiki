56bba1fbcb8207ca89d44ebb76a30a6fef5c2145
admin <kalrobbynson@gmail.com>
2020-11-23 01:29:54 +0300

    hospital bag page

--- lib/views/pages/hospital_bag_page.dart
+++ lib/views/pages/hospital_bag_page.dart
@@ -1,10 +1,35 @@
+import 'package:aunty_rafiki/views/components/tiles/hospital_bag_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HospitalBagPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Hospital Bag')),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(height: 10),
+          HospitalBagTile(
+            icon: FontAwesomeIcons.shoppingBag,
+            onTap: () {},
+            subtitle: "0  items packed",
+            title: "Mother's Bag",
+          ),
+          HospitalBagTile(
+            icon: FontAwesomeIcons.shoppingBag,
+            onTap: () {},
+            subtitle: "0  items packed",
+            title: "Birth partner's Bag",
+          ),
+          HospitalBagTile(
+            icon: FontAwesomeIcons.shoppingBag,
+            onTap: () {},
+            subtitle: "0  items packed",
+            title: "Baby's Bag",
+          ),
+        ]),
+      ),
     );
   }
 }
