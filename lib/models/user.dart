f69f669f01686091727b2b68cd4fff04d498e666
admin <kalrobbynson@gmail.com>
2021-01-29 14:01:23 +0300

    midwife

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -8,6 +8,18 @@ class User {
   final String phoneNumber;
   final String photoUrl;
   final String uid;
+  final int gravida;
+  final int haemoLevel;
+  final DateTime haemoLevelDate;
+  final DateTime lastDateTetanusVaccine;
+  final int numberOfDeliveries;
+  final int numberOfNormalDeliveries;
+  final int numberOfOperationDeliveries;
+  final String onMedication;
+  final int pregnancyWeeks;
+  final String startedClinic;
+  final String takenTetanusVaccine;
+  final int yearOfBirth;
 
   User(
       {@required this.phoneNumber,
@@ -15,6 +27,18 @@ class User {
       this.groups,
       this.nameInitials,
       this.photoUrl,
+      this.gravida,
+      this.haemoLevel,
+      this.haemoLevelDate,
+      this.lastDateTetanusVaccine,
+      this.numberOfDeliveries,
+      this.numberOfNormalDeliveries,
+      this.numberOfOperationDeliveries,
+      this.onMedication,
+      this.pregnancyWeeks,
+      this.startedClinic,
+      this.takenTetanusVaccine,
+      this.yearOfBirth,
       @required this.uid});
 
   User.fromMap(Map<String, dynamic> map)
@@ -23,6 +47,18 @@ class User {
         displayName = map['displayName'],
         nameInitials = map['nameInitials'],
         photoUrl = map['photoUrl'],
+        gravida = map['gravida'],
+        haemoLevel = map['haemoLevel'],
+        haemoLevelDate = map['haemoLevelDate'],
+        lastDateTetanusVaccine = map['lastDateTetanusVaccine'],
+        numberOfDeliveries = map['numberOfDeliveries'],
+        numberOfNormalDeliveries = map['numberOfNormalDeliveries'],
+        numberOfOperationDeliveries = map['numberOfOperationDeliveries'],
+        onMedication = map['onMedication'],
+        pregnancyWeeks = map['pregnancyWeeks'],
+        startedClinic = map['startedClinic'],
+        takenTetanusVaccine = map['takenTetanusVaccine'],
+        yearOfBirth = map['yearOfBirth'],
         groups = map['groups'];
 
   User.fromFirestore(DocumentSnapshot doc)
@@ -31,10 +67,21 @@ class User {
         displayName = doc.data()['displayName'],
         nameInitials = doc.data()['nameInitials'],
         photoUrl = doc.data()['photoUrl'],
+        gravida = doc.data()['gravida'],
+        haemoLevel = doc.data()['haemoLevel'],
+        haemoLevelDate = doc.data()['haemoLevelDate'],
+        lastDateTetanusVaccine = doc.data()['lastDateTetanusVaccine'],
+        numberOfDeliveries = doc.data()['numberOfDeliveries'],
+        numberOfNormalDeliveries = doc.data()['numberOfNormalDeliveries'],
+        numberOfOperationDeliveries = doc.data()['numberOfOperationDeliveries'],
+        onMedication = doc.data()['onMedication'],
+        pregnancyWeeks = doc.data()['pregnancyWeeks'],
+        startedClinic = doc.data()['startedClinic'],
+        takenTetanusVaccine = doc.data()['takenTetanusVaccine'],
+        yearOfBirth = doc.data()['yearOfBirth'],
         groups = doc.data()['groups'];
 }
 
 List<User> firestoreToUserList(QuerySnapshot snapshot) {
   return snapshot.docs.map((doc) => User.fromFirestore(doc)).toList();
 }
-
--- lib/views/screens/chats/private_chats.dart
+++ lib/views/screens/chats/private_chats.dart
@@ -1,3 +1,4 @@
+import 'package:aunty_rafiki/views/components/tiles/no_items.dart';
 import 'package:flutter/material.dart';
 import 'package:aunty_rafiki/constants/routes/routes.dart';
 
@@ -12,85 +13,12 @@ class PrivateChats extends StatefulWidget {
 }
 
 class _PrivateChatsState extends State<PrivateChats> {
-  String _searchText = "";
-
   @override
   Widget build(BuildContext context) {
-    final db = FirebaseFirestore.instance;
-    return SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search...",
-                hintStyle: TextStyle(color: Colors.grey.shade400),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Colors.grey.shade400,
-                  size: 20,
-                ),
-                filled: true,
-                fillColor: Colors.grey.shade100,
-                contentPadding: EdgeInsets.all(8),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide(color: Colors.grey.shade100)),
-              ),
-              onChanged: (val) {
-                setState(() {
-                  _searchText = val;
-                });
-              },
-            ),
-          ),
-          StreamBuilder<List<Chat>>(
-            stream: (_searchText != "" && _searchText != null)
-                ? db
-                    .collection('groups')
-                    .orderBy('name')
-                    .where("searchKeywords", arrayContains: _searchText)
-                    .snapshots()
-                    .map(firestoreToChatList)
-                : db
-                    .collection('groups')
-                    .orderBy('name')
-                    .snapshots()
-                    .map(firestoreToChatList),
-            builder: (context, AsyncSnapshot<List<Chat>> snapshot) {
-              if (snapshot.hasError) {
-                return Center(
-                    child: Text('error: ${snapshot.error.toString()}'));
-              }
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
-              List<Chat> chatList = snapshot.data;
-              return ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: chatList.length,
-                itemBuilder: (context, index) {
-                  return ChatUserTile(
-                    chat: chatList[index],
-                    onTap: () {
-                      Navigator.pushNamed(
-                        context,
-                        chatRoomPage,
-                        arguments: chatList[index],
-                      );
-                    },
-                    isGroup: false,
-                  );
-                },
-              );
-            },
-          ),
-        ],
-      ),
-    );
+    return Center(
+        child: NoItemTile(
+      icon: 'assets/icons/chat.png',
+      title: 'Coming soon !!',
+    ));
   }
 }
