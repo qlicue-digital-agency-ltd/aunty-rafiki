891de1f9cd09476659645bcb00e7a7a3959f6ce2
admin <kalrobbynson@gmail.com>
2021-02-08 20:04:35 +0300

    revamp

--- lib/App.dart
+++ lib/App.dart
@@ -33,7 +33,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'views/pages/home_page.dart';
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
