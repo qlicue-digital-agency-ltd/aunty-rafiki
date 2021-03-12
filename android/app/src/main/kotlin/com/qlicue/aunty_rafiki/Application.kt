846ee93d164f7791bd1268e727417b61b7ecca42
Henry Lyamuya <lyamuyahenry@gmail.com>
2021-03-12 16:46:10 +0300

    android mainfest from henry

--- android/app/src/main/kotlin/com/qlicue/aunty_rafiki/Application.kt
+++ android/app/src/main/kotlin/com/qlicue/aunty_rafiki/Application.kt
@@ -2,18 +2,19 @@ package com.qlicue.aunty_rafiki
 
 import io.flutter.app.FlutterApplication
 import io.flutter.plugin.common.PluginRegistry
-import com.your.application.CloudFirestorePluginRegistrant
+import com.qlicue.aunty_rafiki.CloudFirestorePluginRegistrant
+import com.qlicue.aunty_rafiki.CloudFirestorePluginRegistrant
 
 class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
 
-override fun onCreate() {
-super.onCreate()
-}
+    override fun onCreate() {
+        super.onCreate()
+    }
 
-override fun registerWith(registry: PluginRegistry?) {
-if (registry != null) {
-CloudFirestorePluginRegistrant.registerWith(registry)
-}
-}
+    override fun registerWith(registry: PluginRegistry?) {
+        if (registry != null) {
+            CloudFirestorePluginRegistrant.registerWith(registry)
+        }
+    }
 
 }
--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,4 +1,4 @@
-// const api = "https://aunty-rafiki.qlicue.co.tz/api/";
-const api = "http://127.0.0.1:8000/api/";
+ const api = "https://aunty-rafiki.qlicue.co.tz/api/";
+//const api = "http://127.0.0.1:8000/api/";
 
 
