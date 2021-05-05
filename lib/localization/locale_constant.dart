32d244a342372c7f161a720d392476e499d46383
admin <kalrobbynson@gmail.com>
2021-05-05 18:32:25 +0300

    removal of residue files

--- lib/localization/locale_constant.dart
+++ lib/localization/locale_constant.dart
@@ -1,24 +1,21 @@
- 
 import 'dart:ui';
 
 import 'package:aunty_rafiki/App.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
-
- 
-
 
 const String prefSelectedLanguageCode = "SelectedLanguageCode";
 
 Future<Locale> setLocale(String languageCode) async {
-  // SharedPreferences _prefs = await SharedPreferences.getInstance();
-  // await _prefs.setString(prefSelectedLanguageCode, languageCode);
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  await _prefs.setString(prefSelectedLanguageCode, languageCode);
   return _locale(languageCode);
 }
 
 Future<Locale> getLocale() async {
-  // SharedPreferences _prefs = await SharedPreferences.getInstance();
-  //  _prefs.getString(prefSelectedLanguageCode) ?? "sw";
+  SharedPreferences _prefs = await SharedPreferences.getInstance();
+  _prefs.getString(prefSelectedLanguageCode) ?? "sw";
   String languageCode = "sw";
   return _locale(languageCode);
 }
@@ -33,4 +30,3 @@ void changeLanguage(BuildContext context, String selectedLanguageCode) async {
   var _locale = await setLocale(selectedLanguageCode);
   App.setLocale(context, _locale);
 }
- 
